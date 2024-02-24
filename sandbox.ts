84730c994c8ba167f58190b85ad5d967d2857acd
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2024-02-24 13:43:30 +0530

    Explored basic types in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,16 @@
-const firstname = "Aditya";
-console.log(firstname);
+let firstname = "Aditya";
+let lastName = "Dwivedi";
+let age = 30;
+let isStudent = false;
 
-const inputfields = document.querySelectorAll('input')
-inputfields.forEach(ele => console.log(ele))
+//cannot change the datatype
+// firstname = 50;
+// lastName = false;
+// age = "no Age";
+// isStudent = "Yes";
+
+//can change the values
+firstname = "Sagar";
+lastName = "Dwivedi";
+age = 20;
+isStudent = true;
