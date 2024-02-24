b0c3f46585d60fbe5a9d87e2b7dc08186df9ffa2
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2024-02-24 16:53:44 +0530

    Learned function signature in typescript

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,19 @@
-const firstname = "Aditya";
-console.log(firstname);
+let func : (a:number, b:number) => void
+func = (num1:number, num2:number) => {
+    console.log(num1+num2)
+}
 
-const inputfields = document.querySelectorAll('input')
-inputfields.forEach(ele => console.log(ele))
+let func1 : (a:number, b:number, c:string) => number
+func1 = (num1:number, num2:number, action:string) => {
+ if(action==="add"){
+    return num1+num2
+ }else{
+    return num1-num2
+ }
+}
+
+let func2 : (obj:{name : string, age:number}) => void
+type person = {name : string, age:number}
+func2= (userObj : person) => {
+    console.log(`${userObj.name} is ${userObj.age} years old`);
+}
