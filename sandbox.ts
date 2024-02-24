be40263926f71f0560dc0974486b2d7a94fb07b3
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2024-02-24 14:25:50 +0530

    Learned explicit types in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,37 @@
-const firstname = "Aditya";
-console.log(firstname);
+//explicit types
 
-const inputfields = document.querySelectorAll('input')
-inputfields.forEach(ele => console.log(ele))
+let fname:string;
+let age:number;
+ //age = "luigi";
+ age = 20;
+
+ let uid: string | number;
+ uid = 20;
+ uid = "abc"
+ //uid = true;
+
+ //arrays
+ let arr : number[] = []
+ //arr.push(false);
+ arr.push(20)
+
+ let mixed : (number | string)[] =[]
+ mixed.push(20)
+mixed.push("luigi")
+//mixed.push(true)
+
+//objects
+
+let obj1: object;
+obj1 = {
+    name:"Adi",
+    age:20
+}
+obj1 = []
+
+let obj2:{
+    age:number,
+    name:string
+}
+//obj2 = []
+//obj2 = {age:20, name:"adi", skills:[]}
