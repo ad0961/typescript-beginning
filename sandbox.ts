2b21fcf5d4531935477d003dbffc492b45f20a0f
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2024-02-24 14:06:32 +0530

    Learned about advanced types in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,31 @@
-const firstname = "Aditya";
-console.log(firstname);
+let names = ["Aditya", "Sagar", "Abhishek"];
+names.push("Aman");
+//names.push(20);
+//names[0] = 40;
+//names = "Aditya";
 
-const inputfields = document.querySelectorAll('input')
-inputfields.forEach(ele => console.log(ele))
+let ages = [20,30,40,50];
+ages.push(20);
+//ages.push("Adi");
+//ages[0] = "Sagar";
+
+let mixed = ["Adi", 20, 30, "Sagar"]
+mixed.push(30)
+mixed.push("Abhishek")
+mixed[0] = 70;
+mixed[1] = "Sarthak"
+
+let obj = {
+    name: "Luigi",
+    age: 30,
+    isMarried: false
+}
+
+obj.name="Adi";
+//obj.name=40;
+//obj.hobby = "cricket"
+
+// obj = {
+//     name: "Luigi",
+//     age: 30, 
+// }
