d7fd792cf5064751398c65de6f887c3608deec3f
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2024-02-24 15:12:50 +0530

    Learned function basics and type aliases in typescript

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,40 @@
-const firstname = "Aditya";
-console.log(firstname);
+let fun : Function;
 
-const inputfields = document.querySelectorAll('input')
-inputfields.forEach(ele => console.log(ele))
+//fun = "String";
+fun = () => {
+    console.log("woohoo")
+}
+
+const add = (a:number, b:number, c?:number):void => {
+    console.log(a+b)
+}
+
+add(5,10)
+add(3,4,5)
+
+const sub = (a:number, b:number, c:number | string=2) => {
+    console.log(a+b)
+}
+sub(2,1)
+sub(5,2,1)
+
+const mul = (a:number, b:number, c:number=2) : number => {
+    return a*b*c;
+}
+let num = mul(3,4,5);
+
+//type aliases
+
+type numOrStr = number | string
+type userObj = {
+    name : string,
+    uid : numOrStr
+}
+
+const greet = (user : userObj) => {
+    console.log(`Hi there ${user.name}`)
+}
+
+const logDetails = (user : userObj) => {
+    console.log(`Hi there ${user.uid}`)
+}
